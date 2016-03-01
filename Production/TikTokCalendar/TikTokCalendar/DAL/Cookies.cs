7b8e1cfcab99487a0a6af5d2d2bbfc5a5a27232e
thangphan93 <phatha14@student.westerdals.no>
2016-03-01 12:04:47 +0100

    Add cookie class with save and load

---
+++ Production/TikTokCalendar/TikTokCalendar/DAL/Cookies.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TikTokCalendar.DAL
+{
+	public class Cookies
+	{
+
+		public void SaveToCookie(String UserName)
+		{
+			/*
+			HttpCookie yourCookie = new HttpCookie(UserName);
+			yourCookie.Value = "GetNameFromUserHERE" + DateTime.Now.ToString();
+			yourCookie.Expires = DateTime.Now.AddDays(366);
+			yourCookie.Path = "/TikTokCalendar";
+			*/
+
+		}
+		public void LoadFromCookie(String UserName)
+		{
+			HttpCookie myCookie = new HttpCookie(UserName);
+			myCookie = Request.Cookies[UserName];
+			if (myCookie == null) {
+				SaveToCookie(UserName);
+			}
+		}
+	}
+}
