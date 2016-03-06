ca2cc73688e12eb8a3c98201e54616cb17113d37
Tord <assessedgrub@gmail.com>
2016-03-06 14:26:44 +0100

    Added new structure for the model to pass to the view, added function to get events based on a user

---
+++ Production/TikTokCalendar/TikTokCalendar/DAL/CustomEventMonth.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TikTokCalendar.Extras;
+using TikTokCalendar.Models;
+
+namespace TikTokCalendar.DAL
+{
+	public class CustomEventMonth
+	{
+		public int MonthNumber { get; set; }
+		public List<CustomEventWeek> Weeks { get; set; }
+
+		public CustomEventMonth(int monthNumber)
+		{
+			MonthNumber = monthNumber;
+			Weeks = new List<CustomEventWeek>();
+		}
+
+		public void AddEvent(CustomEvent evnt)
+		{
+			Weeks[evnt.StartDateTime.GetWeekNumberOfYear()].events.Add(evnt);
+		}
+	}
+}
---
+++ Production/TikTokCalendar/TikTokCalendar/DAL/CustomEventWeek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TikTokCalendar.Models;
+
+namespace TikTokCalendar.DAL
+{
+	public class CustomEventWeek
+	{
+		public int WeekNumber { get; private set; }
+		public List<CustomEvent> events { get; set; }
+
+		public CustomEventWeek(int weekNumber)
+		{
+			WeekNumber = weekNumber;
+			events = new List<CustomEvent>();
+		}
+	}
+}
