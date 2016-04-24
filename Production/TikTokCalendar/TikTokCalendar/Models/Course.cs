2c9973941c0288107d448fc8079dbd5c70886add
Tord <assessedgrub@gmail.com>
2016-04-24 15:40:15 +0200

    Removed unecessary lines of code and files

--- Production/TikTokCalendar/TikTokCalendar/Models/Course.cs
+++ Production/TikTokCalendar/TikTokCalendar/Models/Course.cs
@@ -19,19 +19,8 @@ namespace TikTokCalendar.Models
 			Name = name;
 
 			// Parse the name to a course enum
-			// TODO Something is fucky here
-			//int bestMatch = 1000;
 			var course = SchoolCourses.Programmering;
-			//string[] courses = Enum.GetNames(typeof(SchoolCourses));
-			//for (int i = 1; i < courses.Length + 1; i ++)
-			//{
-			//	int match = Math.Abs(name.CompareTo(courses[i - 1]));
-			//	if (match <= bestMatch)
-			//	{
-			//		course = (SchoolCourses)i;
-			//		bestMatch = match;
-			//	}
-			//}
+			
 			// Replace spaces and dashes so it matches the enum
 			// TODO User RegEx
 			var safeName = name.Replace("-", "");
@@ -48,30 +37,7 @@ namespace TikTokCalendar.Models
 			{
 				Debug.WriteLine("WARNING: ERROR: Couldn't parse course enum '" + name + "'!");
 			}
-			//int bestMatch = 1000;
-			//string[] courses = Enum.GetNames(typeof(SchoolCourses));
-			//for (int i = 1; i < courses.Length + 1; i++)
-			//{
-			//	int match = Math.Abs(name.CompareTo(courses[i - 1]));
-			//	if (match <= bestMatch)
-			//	{
-			//		course = (SchoolCourses)i;
-			//		bestMatch = match;
-			//	}
-			//}
 			return course;
 		}
-
-		//}
-		//	return a.ID == b.ID;
-		//	if ((object)a == null || (object)b == null) return false;
-		//{
-
-		//public static bool operator ==(Course a, Course b)
-
-		//public static bool operator !=(Course a, Course b)
-		//{
-		//	return !(a == b);
-		//}
 	}
 }
